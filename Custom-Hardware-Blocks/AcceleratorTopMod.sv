bb5941d275c0acd0e30b3e98ee31d3008580492d
695 ECE <695r07@ecegrid-thin4.ecn.purdue.edu>
2019-11-29 03:01:57 -0500

    Removed system generated files from git repo
    - updated gitignore for Quartus
    - reset tracked files list to trim repo

---
+++ Custom-Hardware-Blocks/AcceleratorTopMod.sv
@@ -0,0 +1,21 @@
+/**
+ *	AcceleratorTopMod.sv
+ *
+ * 	Created	:	26 Nov 2019, 12:44 PM EST
+ * 	Author	:	Abhishek Bhaumick
+ * 	
+ */
+
+
+module AcceleratorTopMod (
+	input 			clk,		// Clock
+	input 			clk_en,	// Clock Enable
+	input			rst,		// Asynchronous reset active low
+
+	input [ 7:0]	addressIn,
+	input [31:0]	dataIn,
+
+	
+);
+
+endmodule
